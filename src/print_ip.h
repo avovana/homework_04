cb0f463031928bfe1270c68fe08c9eb152c55933
Vladimir <vladimir.englishbox@gmail.com>
2018-07-17 21:13:06 +0300

    Refactored

--- src/print_ip.h
+++ src/print_ip.h
@@ -10,7 +10,7 @@
 using byte = std::uint8_t;
 
 template <class T>
-std::enable_if_t<std::is_integral<T>::value, void> print_ip(std::ostream& os, const T &number_)
+std::enable_if_t<std::is_integral<T>::value, void> print_ip(const T &number_, std::ostream& os = std::cout)
 {
 	T number = number_;
 
@@ -34,7 +34,7 @@ std::enable_if_t<std::is_integral<T>::value, void> print_ip(std::ostream& os, co
 }
 
 template <class T>
-std::enable_if_t<is_stl_container<T>::value && std::is_integral<typename T::value_type>::value, void> print_ip(std::ostream& os, const T &container)
+std::enable_if_t<is_stl_container<T>::value && std::is_integral<typename T::value_type>::value, void> print_ip(const T &container, std::ostream& os = std::cout)
 {
 	for (auto it = std::begin(container); it != std::end(container); ++it)
 	{
@@ -53,7 +53,7 @@ struct is_string<std::string> : std::true_type {};
 
 template <typename T>
 typename std::enable_if<is_string<T>::value, void>::type
-print_ip(std::ostream& os, T const & x)
+print_ip(T const & x, std::ostream& os = std::cout)
 {
 	os << x;
 }
